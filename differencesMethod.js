3930897949a310a6e5b8386e75a2fc52fb4f4514
Roberto Rolon <Roberto.Rolon@therigy.local>
2016-02-10 18:03:43 -0500

    Create the method Differences and attach to the prototype of Array

---
+++ differencesMethod.js
@@ -0,0 +1,24 @@
+(function() {
+
+    //this does not work, Array objects do not innately have a differences function. 
+    //however, you can attach methods via the prototype attribute in order to make them accessible to other objects. 
+    //take a look how to do this below.
+    try {
+         [1,2,3].differences([3,4]);        
+    }catch(e) {console.log(e)}
+
+
+    //Attaching this method to the Array.prototype allows other array objects to call this method
+    Array.prototype.differences = function(array) {    
+    var filteredArray = this.filter(function(i) {
+        return array.indexOf(i) == -1;
+    }); 
+
+    return filteredArray;
+}
+
+    //this returns an array object with the values that do not exist in the [3,4] array object
+    console.log([1,2,3].differences([3,4])); 
+    
+    console.log([3,4].differences([1,2,3]));
+})();
